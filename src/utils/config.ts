eb9be3fac0489684a8552953902059b0ae995eab
Jay kumar Pandit <jayactive11112@gmail.com>
2025-09-04 09:42:51 +0530

    tested and modified to previous url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
 // Centralized web configuration
-export const WEB_HOST = 'mynafe-git-feature-notification-enroot-mumbais-projects.vercel.app';
+export const WEB_HOST = 'mynafe.vercel.app';
 export const WEB_URL = `https://${WEB_HOST}`;
 
 
